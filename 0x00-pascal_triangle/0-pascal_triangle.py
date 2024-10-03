ee3dcc7775d41cf8623bfbd6565b5b0598b96b42
Emmanuel Odenyire Anyira <eodenyire@gmail.com>
2024-10-03 16:22:43 +0000

    Fix pycodestyle issues

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,9 +2,9 @@ def pascal_triangle(n):
     """Returns a list of lists representing Pascal's triangle of n."""
     if n <= 0:
         return []
-    
+
     triangle = [[1]]  # Start with the first row
-    
+
     for i in range(1, n):
         prev_row = triangle[-1]  # Get the previous row
         # Generate the new row by adding adjacent elements in the previous row
@@ -13,5 +13,5 @@ def pascal_triangle(n):
             new_row.append(prev_row[j - 1] + prev_row[j])
         new_row.append(1)  # Every row ends with 1
         triangle.append(new_row)
-    
+
     return triangle
