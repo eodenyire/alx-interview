672a644ad7c120082d90bf3c8530155f4cf601b7
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-10-03 19:28:26 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,20 +2,20 @@
 """
 Pascal's Triangle
 """
+
+
 def pascal_triangle(n):
-    """Returns a list of lists representing Pascal's triangle of n."""
+    """Returns a list of integers representing Pascal’s triangle of n."""
     if n <= 0:
-        return []
+        return []  # Return an empty list if n is less than or equal to 0
 
-    triangle = [[1]]  # Start with the first row
+    res = []
+    for i in range(1, n + 1):
+        level = []
+        C = 1
+        for j in range(1, i + 1):
+            level.append(C)
+            C = C * (i - j) // j
+        res.append(level)
 
-    for i in range(1, n):
-        prev_row = triangle[-1]  # Get the previous row
-        # Generate the new row by adding adjacent elements in the previous row
-        new_row = [1]  # Every row starts with 1
-        for j in range(1, len(prev_row)):
-            new_row.append(prev_row[j - 1] + prev_row[j])
-        new_row.append(1)  # Every row ends with 1
-        triangle.append(new_row)
-
-    return triangle
+    return res
